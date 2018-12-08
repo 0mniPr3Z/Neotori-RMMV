0b523097bf1dea564282756bd317b799d471d4a7
Pierre Andre Hernandez <44750488+0mniPr3Z@users.noreply.github.com>
2018-12-08 02:17:05 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include'inc/head.php'; ?>
+    </head>
+    <body style="background-color: black">
+        <script type="text/javascript" src="./js/libs/pixi.js"></script>
+        <script type="text/javascript" src="./js/libs/fpsmeter.js"></script>
+        <script type="text/javascript" src="./js/libs/lz-string.js"></script>
+        
+        <script type="text/javascript" src="./js/libs/jquery-2.1.4.min.js"></script>
+        <script type="text/javascript" src="./js/libs/crypto.sha1.js"></script>
+        <script type="text/javascript" src="./js/libs/socket.io.js"></script>
+        
+        <script type="text/javascript" src="./js/rpg_core.js"></script>
+        <script type="text/javascript" src="./js/rpg_managers.js"></script>
+        <script type="text/javascript" src="./js/rpg_objects.js"></script>
+        <script type="text/javascript" src="./js/rpg_scenes.js"></script>
+        <script type="text/javascript" src="./js/rpg_sprites.js"></script>
+        <script type="text/javascript" src="./js/rpg_windows.js"></script>
+        <script type="text/javascript" src="./js/plugins.js"></script>
+        <script type="text/javascript" src="./js/main.js"></script>
+        });
+        </script>
+    </body>
+</html>
