af19602de6e6ff1a6f49446ec016492441bfc374
Pierre Andre Hernandez <44750488+0mniPr3Z@users.noreply.github.com>
2018-12-08 02:24:19 +0100

    Create head.php include in index

---
+++ inc/head.php
@@ -0,0 +1,12 @@
+<!-- All path modified from absolute to relative! -->
+<meta charset="UTF-8">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<meta name="viewport" content="user-scalable=no">
+<link rel="icon" href="./icon/icon.png" type="image/png">
+<link rel="apple-touch-icon" href="./icon/icon.png">
+<link rel="stylesheet" type="text/css" href="./fonts/gamefont.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+<title>Neotori RPG</title>
